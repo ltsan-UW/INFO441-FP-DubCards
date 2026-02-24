12e65e94ea80be3a685b62c844ea72f2e22269d4
Diego Licea <dlicea@uw.edu>
2026-02-23 18:35:59 -0800

    initial front end javascript commit

---
+++ public/javascripts/index.js
@@ -0,0 +1,16 @@
+async function init(){
+    
+    console.log("loading dubcards frontend...");
+    loadStore();
+}
+
+
+
+
+async function loadStore(){
+
+}
+
+async function loadInventory(){
+
+}
