b0528594345a25e7f1927bcedc4392c1f3e3b6f8
Diego Licea <diegol@Diegos-MacBook-Air-10.local>
2026-02-24 10:14:46 -0800

    added rendering functionality to store sidebar button

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,14 +1,28 @@
 async function init(){
-    
     console.log("loading dubcards frontend...");
     loadStore(); //for now. should load based on buttons later
+    return;
 }
 
-
-
-
 async function loadStore(){
+
+    //eventually
+    //let storeJson = await fetchJSON(`api/${apiVersion}/posts`)
     
+    let postsHtml = 
+         `
+    <div class="store">
+        <h2>Store!</h2>
+        <div class="packs">
+          <div class="cardPack" id="pack1">
+            <strong>Dubs Anniversary Pack</strong>
+            <button>buy</button>
+          </div>
+        </div>
+      </div>
+        `
+    document.getElementById("mainContent").innerHTML = postsHtml;
+    return;
 }
 
 async function loadInventory(){
