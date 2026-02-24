0318a21fd92ad8e4a6869a0a7a6371cd96a42146
Diego Licea <diegol@Diegos-MacBook-Air-10.local>
2026-02-23 23:11:03 -0800

    first store mvp product

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,14 +1,14 @@
 async function init(){
     
     console.log("loading dubcards frontend...");
-    loadStore();
+    loadStore(); //for now. should load based on buttons later
 }
 
 
 
 
 async function loadStore(){
-
+    
 }
 
 async function loadInventory(){
