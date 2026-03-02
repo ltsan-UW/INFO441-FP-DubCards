e22d3b98886dc3077c61b6fd154389846864f709
ltsan-UW <ltsan30@uw.edu>
2026-03-02 09:57:54 -0800

    Added azure auth code and working sign in

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ import { dirname } from 'path';
 import 'dotenv/config';
 import { connectDB } from './app/config/db.js';
 
+
+import { WebAppAuthProvider } from 'msal-node-wrapper';
+import sessions from 'express-session'
+import { env } from 'process';
+const azure_secret = process.env.AZURE_AUTH_SECRET;
+const azure_client_id = process.env.AZURE_CLIENT_ID;
+const azure_authority_id = process.env.AZURE_AUTHORITY_ID;
+
 connectDB();
 
 const __filename = fileURLToPath(import.meta.url);
@@ -24,6 +32,39 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.enable('trust proxy')
+const oneDay = 1000 * 60 * 60 * 24;
+app.use(sessions({
+    secret: azure_secret,
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
+    resave: false
+  }));
+const authProvider = await WebAppAuthProvider.initialize({
+    auth: {
+        authority: "https://login.microsoftonline.com/" + azure_authority_id,
+        clientId: azure_client_id,
+        clientSecret: azure_secret,
+        redirectUri: "/redirect",
+    }
+});
+app.use(authProvider.authenticate({
+    protectAllRoutes: false, // force user to authenticate for all routes
+    acquireTokenForResources: { // acquire an access token for this resource
+        "graph.microsoft.com": { // you can specify the resource name as you like
+            scopes: ["User.Read"], // scopes for the resource that you want to acquire a token for
+            routes: ["/profile"] // acquire a token before the user hits these routes
+        },
+    }
+}));
+app.use(authProvider.interactionErrorHandler()); // this middleware handles interaction required errors
+
+// unauthorized
+app.get('/error', (req, res) => res.status(500).send('server error'));
+// error
+app.get('/unauthorized', (req, res) => res.status(401).send('Permission denied'));
+
 
 app.use('/api', apiRouter);
 app.use('/auth', authRouter);
--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -4,8 +4,8 @@ var router = express.Router();
 import * as authController from '../controllers/auth.controller.js';
 
 router.post('/register', authController.register);
-router.post('/signin', authController.signIn);
-router.post('/signout', authController.signOut);
+router.get('/signin', authController.signIn);
+router.get('/signout', authController.signOut);
 
 
 export default router;
