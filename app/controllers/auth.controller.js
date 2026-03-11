250d00f04173f0cdd69d46516990bb4814e18269
ltsan-UW <ltsan30@uw.edu>
2026-03-11 08:51:49 -0700

    Fixed new user having no currency

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -21,7 +21,7 @@ export async function callback(req, res) {
                 username: username,
                 email: account.username,
                 password: "",
-                currency: 0,
+                currency:  500,
                 createdAt: Date.now(),
                 inventory: [],
                 favorites: [],
